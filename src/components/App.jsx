333ef6872a4481305bc4f7222cf4c77ac6b06792
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-12 18:09:28 -0500

    se crea el enrutamiento entre los componentes

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Menu from "./Menu";
+import Usuarios from "./Usuarios";
+
+// const Tareas = () => <div>Tareas</div>;
+const Tareas = () => {
+  return <div>Tareas</div>;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={Usuarios} />
+        <Route exact path="/tareas" component={Tareas} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Usuarios/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Usuarios() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((resp) => {
+        setUsers(resp.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="margen">
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(( user ) => {
+            return (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.website}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default Usuarios;
